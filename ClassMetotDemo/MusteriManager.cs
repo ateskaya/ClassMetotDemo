d2195696ba2ac40eee8ff0c3172c69edcb15e5ca
ensar <ensar@DESKTOP-PJ52DT8>
2021-02-08 20:57:38 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Tc+"TC kimlik numaralı"+musteri.Ad+" "+musteri.Soyad+"isimli"+musteri.TelefonNo+"numaralı kişi kaydedilmiştir.");
+            Console.ReadKey();
+        }
+        public void Kaldir(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Tc + "TC kimlik numaralı" + musteri.Ad + " " + musteri.Soyad + "isimli" + musteri.TelefonNo + "numaralı kişi kaldırılmıştır.");
+            Console.ReadKey();
+        }
+        public void MusteriList(Musteri[] musteriler)
+        {
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("Müşteri TC kimlik no: "+musteri.Tc);
+                Console.WriteLine("Müşterinin adı ve soyadı: "+musteri.Ad+" "+musteri.Soyad);
+                Console.WriteLine("Müşteri tel no: "+musteri.TelefonNo);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Tc = 12345678910;
+            musteri1.Ad = "Ahmet";
+            musteri1.Soyad = "Boz";
+            musteri1.TelefonNo = 5346875215;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Tc = 12365478912;
+            musteri2.Ad = "Mehmet";
+            musteri2.Soyad = "Çukur";
+            musteri2.TelefonNo = 5358156482;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            Console.WriteLine("  Hoş geldiniz  ");
+            Console.WriteLine("  Müşteriler Listeleniyor  ");
+            musteriManager.MusteriList(musteriler);
+            Console.WriteLine("  Lütfen 'Enter' tuşuna basın ve müşteriyi eklediğimi görün.  ");
+            Console.ReadKey();
+            Console.WriteLine("  Müşteri Ekleniyor  ");
+            musteriManager.Ekle(musteri1);
+            Console.WriteLine("  Lütfen 'Enter' tuşuna basın ve müşteriyi kaldırdığımı görün. ");
+            Console.ReadKey();
+            Console.WriteLine("  Müşteri Kaldırılıyor ");
+            musteriManager.Kaldir(musteri2);
+        }
+    }
+}
